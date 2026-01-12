dbffadc7c4eb0e1acd908a111c7938f954d7797b
Aditya Kumar Singh <simghaditya21@gmail.com>
2026-01-12 16:47:56 +0530

    Class 1

---
+++ Class1.py
@@ -0,0 +1,66 @@
+print("Hello world");print("Testing Lines")
+
+# print("Hello world"):print("Testing Lines") gives error
+
+print("Hello world"),print("Testing Lines")
+
+# Identation
+
+if 5 >2:
+    print("Fiver is greater than 2")
+    if 3 >2:
+        print("Three is greater than 2")
+
+# Type 
+
+a= 4 
+A = "STR"
+
+print(type(a))
+print(type(A))
+
+# Variable name 
+#2myvar = something
+#my-var = something
+#my var = something
+
+
+# applicable 
+
+myvar2 = "Ceter for AI"
+my_var = "Ceter for AI"
+myVar = "Ceter for AI"
+
+# Many values getting assigned 
+x,y,z = "123","456","789"
+print(x)
+print(y)
+print(z)
+
+# Multiple Print
+ 
+print(x,y,z)
+
+# Data types
+ 
+# List 
+
+list1 = ["Data","Process","Display"]
+print(list1)
+list1.append("Data")
+print(list1)
+print(len(list1))
+
+# List has varous methods like append , pop , sort ,reverse
+
+print(type(list1))
+
+# Tuples 
+
+tp1 = ("Data","Process","Deploy")
+print(type(tp1))
+
+# Sets 
+
+set1 = {"Data","Process","Deploy"}
+print(set1)
